e676bc130db0326457e1400924edc1738d1a6c3e
Nokome Bentley <nokome@stenci.la>
2020-02-05 21:23:50 +1300

    chore(Pipe): Improve error handling

--- src/pipe.cpp
+++ src/pipe.cpp
@@ -1,10 +1,16 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <cstdio>
 #include <string>
 
 //' Create a named pipe
 //' @param name Name (i.e. file path) for the pipe
 // [[Rcpp::export]]
 bool pipe_make(std::string name) {
-  mkfifo(name.c_str(), 0600);
+  int error = mkfifo(name.c_str(), 0600);
+  if (error != 0) {
+    std::perror("Error in pipe_make");
+    return false;
+  }
+  return true;
 }
